5c27d7c05b9c6805bb19eb84c25609753547467b
tanyapyndiah <tppyndiah@me.com>
2025-06-07 22:40:27 -0400

    added basename

--- devoir2/src/App.jsx
+++ devoir2/src/App.jsx
@@ -9,7 +9,7 @@ function App() {
 
   return (
     
-    <BrowserRouter>
+    <BrowserRouter basename='/Autorepair_site'>
         <Routes>
           <Route path="/" element={<MainPage />}/>
         </Routes>
