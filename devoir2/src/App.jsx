476c93c82e82b37932ae0e29bcfee0cff1385ef1
tanyapyndiah <tppyndiah@me.com>
2025-06-07 22:01:57 -0400

    Removed browserRouter

--- devoir2/src/App.jsx
+++ devoir2/src/App.jsx
@@ -1,7 +1,7 @@
 import './App.css';
 import MainPage from "./MainPage";
 import Navbar from "./components/Navbar";
-import { BrowserRouter as Router, Routes, Route, BrowserRouter } from "react-router-dom";
+import {  Routes, Route, BrowserRouter } from "react-router-dom";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
@@ -10,11 +10,12 @@ function App() {
 
   return (
     
-    <BrowserRouter basename='Autorepair_site'>
-        <Routes>
-          <Route path="/" element={<MainPage />}></Route>
-        </Routes>
-    </BrowserRouter>
+    // <BrowserRouter basename='Autorepair_site'>
+    //     <Routes>
+    //       <Route path="/" element={<MainPage />}></Route>
+    //     </Routes>
+    // </BrowserRouter>
+    <MainPage/>
     
   )
 }
